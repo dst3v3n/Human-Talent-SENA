ec5831949bb8f850ef0b9e10f129afe35d6a7ba6
lauraroseroochoa <lauratatianarosero1@gmail.com>
2023-06-20 07:39:10 -0500

    aporte de laura

--- prueba.py
+++ prueba.py
@@ -10,6 +10,7 @@ print ("Hello Josue")
 ### LAURA ###
 
 print ("Hello Laura")
+print('Los individuos marcan goles, pero los equipos ganan partidos\n','Llegar juntos es el principio. Mantenerse juntos, es el progreso.')
 
 ### Zully ###
 
