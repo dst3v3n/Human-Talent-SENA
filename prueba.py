c97f083c518a35698ff4810302a9769885ff3b99
dst3v3n <hablack10@gmail.com>
2023-06-20 13:48:13 -0500

    Correcion laura

--- prueba.py
+++ prueba.py
@@ -10,7 +10,6 @@ print ("Hello Josue")
 ### LAURA ###
 
 print ("Hello Laura")
-print('Los individuos marcan goles, pero los equipos ganan partidos\n','Llegar juntos es el principio. Mantenerse juntos, es el progreso.')
 
 ### Zully ###
 
