cf1f79768550f9bec2cdb4c0791e332e051fb1c0
dst3v3n <hablack10@gmail.com>
2023-06-25 00:34:15 -0500

    modulo Postulacion terminado

--- Postulacion/Educacion.py
+++ Postulacion/Educacion.py
@@ -14,6 +14,37 @@ class Educacion:
         self.__titulacion.append (Titulacion)
         self.__fecha_fin.append (Fecha_fin)
 
+    
+    ### Setters ###
+
+    def setTipo (self, Name:str , Actualizacion:str):
+        for i in self.__tipo:
+            if Name == i:
+                indice = self.__tipo.index(i)
+                self.__tipo.pop(indice)
+                self.__tipo.insert(indice , Actualizacion)
+
+    def setName (self, Name:str , Actualizacion:str):
+        for i in self.__name_institucion:
+            if Name == i:
+                indice = self.__name_institucion.index(i)
+                self.__name_institucion.pop(indice)
+                self.__name_institucion.insert(indice , Actualizacion)
+    
+    def setTitulacion (self, Name:str , Actualizacion:str):
+        for i in self.__titulacion:
+            if Name == i:
+                indice = self.__titulacion.index(i)
+                self.__titulacion.pop(indice)
+                self.__titulacion.insert(indice , Actualizacion)
+    
+    def setFecha_fin  (self, fecha_anterior:str , Actualizacion:str):
+        for i in self.__fecha_fin:
+            if fecha_anterior == i:
+                indice = self.__fecha_fin.index(i)
+                self.__fecha_fin.pop(indice)
+                self.__fecha_fin.insert(indice , Actualizacion)
+
     ### Getters ###
 
     @property
